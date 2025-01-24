9baa06a05f3a4b3793f2464146514b231fff4640
darshanmandi <98580060+darshanmandi@users.noreply.github.com>
2025-01-24 07:58:27 +0000

    STRING

---
+++ basic/datatype.js
@@ -0,0 +1,5 @@
+let name="Darshan Thakur";
+let age="33";
+let valueage=Number(age)
+console.log(age)
+console.log(valueage)
---
+++ basic/variable.js
@@ -0,0 +1,32 @@
+const Custid=22523;
+let custname="Ritesh";
+var city="Mandi";
+state="Himachal Pradesh";
+console.table([Custid,custname,city,state]);
+console.log(2**3);
+/*et counter =100;
+let a =++counter;
+let b =counter++;
+console.log(counter);
+console.log(a);
+console.log(b);
+*/
+/*const id=Symbol('123')
+const id2=Symbol('123')
+console.log(id==id2)
+const bignumber=1234569258n
+console.log(typeof bignumber);*/
+let fname=" Darshan ";
+let lname =" thakur ";
+console.log(`Hello my  first name is ${fname} and last name is  ${lname}`);
+const test= new String('Darshan')
+console.log(test[0]);
+console.log(test.length);
+console.log(test.toUpperCase());
+console.log(test.toLowerCase());
+console.log(test.charAt(3));
+console.log(test.indexOf('s'));
+console.log(test.substring(0,4));
+console.log(test.trim());
+
+console.log(test.slice(-7,2));
